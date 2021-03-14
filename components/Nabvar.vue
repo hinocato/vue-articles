e73b8595803db3b702a3128010fd6e55576443f8
hinocato <hinocato@gmail.com>
2021-03-14 19:02:58 +0900

    add top-navbar

--- components/Nabvar.vue
+++ components/Nabvar.vue
@@ -1,31 +1,35 @@
 <template>
   <el-menu class="top-navbar" mode="horizontal" background-color="#f08080" text-color="#f0f8ff" active-text-color="#f0f8ff">
-  <h2 class="top-navbar-title">Articles.hinocato.com</h2>
-  <el-menu-item index="1" class="dock-right">About me</el-menu-item>
-  <el-submenu index="2" class="dock-right">
+  <el-menu-item index="/" class="top-navbar-title">articles.hinocato.com</el-menu-item>
+  <el-submenu index="/aboutme" class="dock-right">
     <template slot="title">SourceCode</template>
-    <el-menu-item index="2-1">Web</el-menu-item>
+    <el-menu-item index="2-1"><a href="https://github.com/hinocato/vue-articles" target="_blanck">Web</a></el-menu-item>
     <el-menu-item index="2-2">Api</el-menu-item>
   </el-submenu>
+  <el-menu-item index="3" class="dock-right">About me</el-menu-item>
   </el-menu>
 </template>
 
 <script>
 </script>
 
-<style scoped>
+<style>
 .top-navbar {
   position: fixed;
   width: 100%;
   min-height: 60px;
 }
 
-.el-menu-horizontal > .el-menu-item.dock-right {
+.top-navbar-title {
+    color: #f0f8ff;
+    font-size: 20px;
+}
+
+.el-menu--horizontal > .el-menu-item.dock-right {
     float: right;
 }
 
-.top-navbar-title {
-    color: #f0f8ff;
-    padding: 3px;
+.el-menu--horizontal > .el-submenu.dock-right {
+    float: right;
 }
 </style>
